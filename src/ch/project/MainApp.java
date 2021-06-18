d733fcca38cb4614be7594f865c01007d00b51dd
Igor Fontenele <igorfelias09@gmail.com>
2021-06-18 12:33:59 -0300

    ###Added - project part 1

---
+++ src/ch/project/MainApp.java
@@ -0,0 +1,54 @@
+package ch.project;
+
+import java.io.IOException;
+
+import ch.project.MainApp;
+import ch.project.controller.RootLayoutController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+	private Stage primaryStage;
+	private AnchorPane rootLayout;
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Projeto Integração");
+        
+        initRootLayout();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	public void initRootLayout() {
+		try {
+			// Carrega o root layout do arquivo fxml.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+	        rootLayout = (AnchorPane) loader.load();
+	        
+	        // Mostra a scene (cena) contendo o root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            
+          //Dá ao controller acesso ao main app.
+            RootLayoutController controller = loader.getController();
+            controller.setMainApp(this);
+            
+            primaryStage.show();
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+        
+	}
+}
